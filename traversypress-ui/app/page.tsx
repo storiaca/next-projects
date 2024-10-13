e69d4141c021ba85f0a28e528c4fd90f3d6b4620
Aleksandar Ristic <storiaca@gmail.com>
2024-10-13 17:50:31 +0200

    Make cards dynamic

--- traversypress-ui/app/page.tsx
+++ traversypress-ui/app/page.tsx
@@ -1,11 +1,30 @@
-import { Button } from "@/components/ui/button";
+import DashboardCard from "@/components/dashboard/DashboardCard";
+import { Folder, MessageCircle, Newspaper, User } from "lucide-react";
 export default function Home() {
   return (
     <>
-      <h1 className="text-2xl">Dashboard</h1>
-      <Button variant="destructive" size="lg" className="text-blue-400">
-        Click Me
-      </Button>
+      <div className="flex flec-col md:flex-row justify-between gap-5 mb-5">
+        <DashboardCard
+          title="Posts"
+          count={100}
+          icon={<Newspaper className="text-slate-500" size={72} />}
+        />
+        <DashboardCard
+          title="Categories"
+          count={12}
+          icon={<Folder className="text-slate-500" size={72} />}
+        />
+        <DashboardCard
+          title="Users"
+          count={750}
+          icon={<User className="text-slate-500" size={72} />}
+        />
+        <DashboardCard
+          title="Comments"
+          count={1200}
+          icon={<MessageCircle className="text-slate-500" size={72} />}
+        />
+      </div>
     </>
   );
 }
