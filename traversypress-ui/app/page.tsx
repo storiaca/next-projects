fd124b217364e672fad669111c809da941604621
Aleksandar Ristic <storiaca@gmail.com>
2024-10-14 18:49:25 +0200

    Posts table

--- traversypress-ui/app/page.tsx
+++ traversypress-ui/app/page.tsx
@@ -1,4 +1,5 @@
 import DashboardCard from "@/components/dashboard/DashboardCard";
+import PostsTable from "@/components/posts/PostsTable";
 import { Folder, MessageCircle, Newspaper, User } from "lucide-react";
 export default function Home() {
   return (
@@ -25,6 +26,7 @@ export default function Home() {
           icon={<MessageCircle className="text-slate-500" size={72} />}
         />
       </div>
+      <PostsTable title="Latest Posts" />
     </>
   );
 }
