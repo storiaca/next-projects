db5fa6052fcd4656aca811e28f034f29db094a85
Aleksandar Ristic <storiaca@gmail.com>
2025-09-09 09:03:18 +0200

    Server and client components

---
+++ next15-ts/app/about/page.tsx
@@ -0,0 +1,10 @@
+import Button from "../components/Button";
+
+export default function AboutPage() {
+  return (
+    <div>
+      <h1 className="text-2xl">About page</h1>
+      <Button />
+    </div>
+  );
+}
---
+++ next15-ts/app/components/Button.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Button() {
+  return (
+    <div>
+      <button onClick={() => alert("hello")}>Click alert</button>
+    </div>
+  );
+}
