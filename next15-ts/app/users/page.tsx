b595e78c9aedde9d9601029d04546a8dcc9e9206
Aleksandar Ristic <storiaca@gmail.com>
2025-09-10 13:26:46 +0200

    Fetch all users

--- next15-ts/app/users/page.tsx
+++ next15-ts/app/users/page.tsx
@@ -1,13 +1,39 @@
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Users page",
   description: "Users page description",
 };
-export default function UsersPage() {
+
+async function fetchUsers() {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/users/`, {
+    headers: {
+      "you-api-key": "bla-bla-tajna",
+    },
+  });
+  const user = await response.json();
+
+  return user;
+}
+
+export default async function UsersPage() {
+  const users = await fetchUsers();
+  // console.log(users);
+
   return (
     <div>
       <h1>Users Page</h1>
+      {users.map((user: any) => {
+        return (
+          <div key={user.id}>
+            <p>Name: {user?.name}</p>
+            <p>Email: {user?.email}</p>
+            <p>Phone: {user?.phone}</p>
+            <Link href={`users/${user.id}`}>See Details</Link>
+          </div>
+        );
+      })}
     </div>
   );
 }
