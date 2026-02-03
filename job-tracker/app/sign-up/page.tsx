111ec5a9fee3e2ee3bc0755767605f14a4adeb6c
Aleksandar Ristic <storiaca@gmail.com>
2026-02-03 08:45:16 +0100

    Install Card shadcn

--- job-tracker/app/sign-up/page.tsx
+++ job-tracker/app/sign-up/page.tsx
@@ -1,7 +1,9 @@
+"use client"
+
 export default function SignUpPage() {
   return(
-    <>
+    <div className="flex min-h-[calc(100vh-4rem)] items-center justify-center bg-white p-4">
       <h1>Sign Up</h1>
-    </>
+    </div>
   )
 }
