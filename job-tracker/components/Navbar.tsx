ba52f8c67e75ecdd86ffb9dff8468b845faa4bf4
Aleksandar Ristic <storiaca@gmail.com>
2026-02-06 15:01:31 +0100

    Add session in Navbar.tsx

--- job-tracker/components/Navbar.tsx
+++ job-tracker/components/Navbar.tsx
@@ -1,20 +1,51 @@
 import { Briefcase } from "lucide-react";
 import Link from "next/link";
 import { Button } from "./ui/button";
+import { getSession } from "@/lib/auth/auth";
 
-export default function Navbar() {
-  return(
+export default async function Navbar() {
+  const session = await getSession();
+  return (
     <nav className="border boder-gray-200 bg-white">
       <div className="container mx-auto flex h-16 items-center px-4 justify-between">
-        <Link href="/" className="flex items-center space-x-2 gap-2 text-lg font-semibold text-primary">
+        <Link
+          href="/"
+          className="flex items-center space-x-2 gap-2 text-lg font-semibold text-primary"
+        >
           <Briefcase />
           Job Tracker
         </Link>
-      <div className="flex ites-center gap-4">
-        <Link href="/sign-in"><Button variant="ghost" className="text-gray-700 cursor-pointer hover:text-black">Log In</Button></Link>
-        <Link href="/sign-up"><Button className="bg-primary cursor-pointer hover:bg-primary/90">Start for free</Button></Link>
-      </div>
+        <div className="flex ites-center gap-4">
+          {session?.user ? (
+            <>
+              <Link href="/dashboard">
+                <Button
+                  variant="ghost"
+                  className="text-gray-700 cursor-pointer hover:text-black"
+                >
+                  Dashboard
+                </Button>
+              </Link>
+            </>
+          ) : (
+            <>
+            <Link href="/sign-in">
+            <Button
+              variant="ghost"
+              className="text-gray-700 cursor-pointer hover:text-black"
+            >
+              Log In
+            </Button>
+          </Link>
+          <Link href="/sign-up">
+            <Button className="bg-primary cursor-pointer hover:bg-primary/90">
+              Start for free
+            </Button>
+          </Link></>
+          )}
+          
+        </div>
       </div>
     </nav>
-  )
+  );
 }
--- job-tracker/lib/auth/auth.ts
+++ job-tracker/lib/auth/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { MongoClient } from "mongodb";
+import { headers } from "next/headers";
 
 const client = new MongoClient(process.env.MONGODB_URI!)
 const db = client.db()
@@ -14,8 +15,9 @@ export const auth = betterAuth({
   }
 })
 
-export async function getSession(params:type) {
+export async function getSession() {
   const result = await auth.api.getSession({
     headers: await headers()
   })
+  return result
 }
