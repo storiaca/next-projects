b584622e55abc1021fd4953a784856cb9a069cfc
Aleksandar Ristic <storiaca@gmail.com>
2026-02-09 18:13:04 +0100

    Model interface finished

--- job-tracker/lib/models/Board.ts
+++ job-tracker/lib/models/Board.ts
@@ -1,5 +1,9 @@
 import mongoose, { Schema, Document } from "mongoose";
 
 export interface IBoard extends Document {
-  name: string
+  name: string;
+  userId: string;
+  columns: mongoose.Types.ObjectId[];
+  createdAt: Date;
+  updateAt: Date;
 }
