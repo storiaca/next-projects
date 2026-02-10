f22557c27054eb888ac9eb7b91ea41b74bfaca69
Aleksandar Ristic <storiaca@gmail.com>
2026-02-10 12:21:49 +0100

    Create index for export models

---
+++ job-tracker/lib/models/index.ts
@@ -0,0 +1,7 @@
+import "./Board";
+import "./Column";
+import "./JobApplications";
+
+export { default as Board } from "./Board";
+export { default as Column } from "./Column";
+export { default as JobApplication } from "./JobApplications";
