201afb7965a88d9378eafd2196dc189b1970134f
Aleksandar Ristic <storiaca@gmail.com>
2026-02-13 08:40:34 +0100

    Proxy file finished

--- job-tracker/proxy.ts
+++ job-tracker/proxy.ts
@@ -3,7 +3,12 @@ import { getSession } from "./lib/auth/auth";
 
 export default async function proxy(request: NextRequest) {
   const session = await getSession()
-  if(!session?.user) {
+
+  const isDashboard = request.nextUrl.pathname.startsWith('/dashboard')
+
+  if(isDashboard && !session?.user) {
     return NextResponse.redirect(new URL("/sign-in", request.url))
   }
+
+  return NextResponse.next()
 }
