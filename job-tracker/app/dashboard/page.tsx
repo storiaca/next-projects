345ceb81804d28e157e3426308690c28d6fb4566
Aleksandar Ristic <storiaca@gmail.com>
2026-02-15 11:07:28 +0100

    Get user from Board collection

--- job-tracker/app/dashboard/page.tsx
+++ job-tracker/app/dashboard/page.tsx
@@ -1,4 +1,6 @@
 import { getSession } from "@/lib/auth/auth"
+import connectDB from "@/lib/db"
+import { Board } from "@/lib/models"
 import { redirect } from "next/navigation"
 
 export default async function DashboardPage() {
@@ -7,6 +9,14 @@ export default async function DashboardPage() {
   if(!session?.user) {
     redirect("/sign-in")
   }
+
+  await connectDB()
+
+  await Board.findOne({
+    userId: session.user.id,
+    name: "Job Hunt"
+  })
+
   return(
     <>
       <h1>Dashboard Page</h1>
