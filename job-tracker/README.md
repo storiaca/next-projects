d3755b352789ca98c89c6687622ffb822c3b6c30
Aleksandar Ristic <storiaca@gmail.com>
2026-02-16 09:42:10 +0100

    Create column config valiable

--- job-tracker/README.md
+++ job-tracker/README.md
@@ -12,6 +12,10 @@
 
 <https://www.better-auth.com/docs>
 
+## dnd kit
+
+<https://dndkit.com/>
+
 This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
 
 ## Start mongodb local
--- job-tracker/components/KanbanBoard.tsx
+++ job-tracker/components/KanbanBoard.tsx
@@ -1,9 +1,36 @@
 "use client"
 
-type KanbanBoardProps = {
-  board: "",
+import { Board } from "@/lib/models/models.types"
+import { Award, Calendar, CheckCircle2, Mic, XCircle } from "lucide-react";
+
+interface KanbanBoardProps {
+  board: Board,
   userId: string
 }
+
+const COLUMN_CONFIG: Array<{color: string; icon: React.ReactNode}> = [
+  {
+    color: "bg-cyan-500",
+    icon: <Calendar className="h-4 w-4" />
+  },
+  {
+    color: "bg-purple-500",
+    icon: <CheckCircle2 className="h-4 w-4" />,
+  },
+  {
+    color: "bg-green-500",
+    icon: <Mic className="h-4 w-4" />,
+  },
+  {
+    color: "bg-yellow-500",
+    icon: <Award className="h-4 w-4" />,
+  },
+  {
+    color: "bg-red-500",
+    icon: <XCircle className="h-4 w-4" />,
+  },
+]
+
 export default function KanbanBoard({board, userId}: KanbanBoardProps) {
   return (
     <>
